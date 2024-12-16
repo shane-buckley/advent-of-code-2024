0b4aa213aa614911d41f63fe97bc42c6f22539cd
shane-buckley1 <shane.buckley1@ucdconnect.ie>
2024-12-16 19:43:29 +0000

    Added method for creating each Day's files (solution, input, testInput)

--- src/Utils.kt
+++ src/Utils.kt
@@ -1,7 +1,6 @@
 import java.math.BigInteger
 import java.security.MessageDigest
-import kotlin.io.path.Path
-import kotlin.io.path.readText
+import kotlin.io.path.*
 
 /**
  * Reads lines from the given input txt file.
@@ -19,3 +18,23 @@ fun String.md5() = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteA
  * The cleaner shorthand for printing output.
  */
 fun Any?.println() = println(this)
+
+fun main() {
+    (1..3).forEach {
+        val dayString = it.toString().padStart(2, '0')
+        val solutionFile = Path("src/Day$dayString.kt")
+        if (!solutionFile.exists()) {
+            solutionFile.createFile()
+            var template = Path("src/DayXX.kt").readLines()
+            template = template.map { line ->
+                line.replace("XX", dayString)
+            }
+            solutionFile.writeLines(template)
+        }
+        val inputFile = Path("src/Day$dayString.txt")
+        if (!inputFile.exists()) inputFile.createFile()
+
+        val testInputFile = Path("src/Day${dayString}_test.txt")
+        if (!testInputFile.exists()) testInputFile.createFile()
+    }
+}
